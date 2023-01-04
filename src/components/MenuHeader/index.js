03c0749ecbe8f7dcfe393918b7b051ad84b3db30
Nagendar-wtc <nagendar.bondaladinne@walkingtree.tech>
2023-01-04 14:57:59 +0530

    Menu header component UI code changes!!

--- src/components/MenuHeader/index.js
+++ src/components/MenuHeader/index.js
@@ -1,90 +1,81 @@
 import React from "react";
-import "./style.scss";
-import TabMenuComponent from "../ui/TabMenuComponent";
-import { ReactComponent as TurbinWings } from ".././../assets/images/turbine-wings.svg";
-import { ReactComponent as GELogo } from ".././../assets/images/GE-logo.svg";
-import { ReactComponent as Blocked } from ".././../assets/images/Blocked.svg";
-import { ReactComponent as RightArrow } from ".././../assets/images/right-arrow.svg";
-import DropdownMenu from "../ui/DropdownMenu";
 import { useNavigate } from "react-router-dom";
+
+//@custom components
 import OverLay from "../OverLay";
+import DropdownMenu from "../ui/DropdownMenu";
+import TabMenuComponent from "../ui/TabMenuComponent";
 
+//@custom styles
+import "./style.scss";
 
+//@assets
+import { ReactComponent as GELogo } from ".././../assets/images/GE-logo.svg";
+
+//@functional component
 const MenuHeader = (props) => {
-  let navigate=useNavigate();
-  let activeTabIndex = 0;
+  let items = [];
+  let navigate = useNavigate();
+  let activeTabIndex = (location.pathname == "/history-page") ? 1 : 0;
+
   let {
-    tabOptions = [],
     handleChange,
+    tabOptions = [],
     setConfigurationOverlay,
     showConfigurationOverlay
   } = props;
-  let items = [];
+
   tabOptions.forEach((item) => {
     items.push({ label: item });
   });
-
-  if(window.location.pathname=="/landing-page")
-    {
-      activeTabIndex=0;
-    }else if(window.location.pathname=="/history-page")
-    {
-      activeTabIndex=1;
-    }
 
   const handleTabChange = (event) => {
     handleChange(event);
   };
 
-  let DropdownList = [
-    {
-        label: 'Profile',
-        command:()=>{ navigate("/Profile");}
-    },
-    {
-        label: 'About Us',
-        command:()=>{navigate("/AboutUs");}
-    },
-    {
-        label: 'Help',
-        command:()=>{navigate("/Help");}
-    },
-    {
-        label: 'Logout',
-        icon: 'pi pi-sign-in'
-    }
-];
+  let DropdownList = [{
+    label: 'Profile',
+    command: () => { navigate("/Profile"); }
+  }, {
+    label: 'About Us',
+    command: () => { navigate("/AboutUs"); }
+  }, {
+    label: 'Help',
+    command: () => { navigate("/Help"); }
+  }, {
+    label: 'Logout',
+    icon: 'pi pi-sign-in'
+  }];
+
   return (
     <>
-    <div className="menu-header" data-testid="menu-header">
-      <div className="menu-tab">
-        <div className="ge-logo-wrapper">
-          <GELogo className="ge-logo" />
-         </div>
-         <span className='logo-divider'></span>
-        <TabMenuComponent
-          tabOptions={items}
-          onChange={handleTabChange}
-          activeIndex={activeTabIndex}
-        />
-      </div>
-      <div className="menu-profile">
-        <span className="blocked-list" onClick={() => setConfigurationOverlay(true)} style={{cursor:"pointer"}}>
-          {/* <TurbinWings className="turbine-wing-icon"/> */}
-          {/* 200 <Blocked className="blocked-icon"/> */}
-          {/* 4Blocked <RightArrow className="right-icon" /> */}
-        </span>
-        <span className="vertical-divider" />
-        <div>
-          <DropdownMenu DropdownName={"Simon Saez"} lists={DropdownList} />
+      <div className="menu-header" data-testid="menu-header">
+        <div className="menu-tab">
+          <div className="ge-logo-wrapper">
+            <GELogo className="ge-logo" />
+          </div>
+          <span className='logo-divider'></span>
+          <TabMenuComponent
+            tabOptions={items}
+            onChange={handleTabChange}
+            activeIndex={activeTabIndex}
+          />
+        </div>
+        <div className="menu-profile">
+          <span className="blocked-list" onClick={() => setConfigurationOverlay(true)} style={{ cursor: "pointer" }}>
+          </span>
+          <span className="vertical-divider" />
+          <div>
+            <DropdownMenu DropdownName={"Simon Saez"} lists={DropdownList} />
+          </div>
         </div>
       </div>
-    </div>
-    <OverLay
-      showConfigurationOverlay={showConfigurationOverlay}
-      setConfigurationOverlay={setConfigurationOverlay}
-    />
-  </>
+      <OverLay
+        showConfigurationOverlay={showConfigurationOverlay}
+        setConfigurationOverlay={setConfigurationOverlay}
+      />
+    </>
   );
 };
+
 export default MenuHeader;
